50cdaf70083d91e50d1155be236b6ff6c6a59980
Bob <vovaoleshko05@mail.ru>
2025-10-09 15:26:54 +0300

    add handler

--- internal/handlerJSON/handlerJson.go
+++ internal/handlerJSON/handlerJson.go
@@ -3,6 +3,7 @@ package handlerJSON
 import (
 	"github.com/gin-gonic/gin"
 
+	h "github.com/vova1001/Website-Ylia-fitness/internal/handler"
 	m "github.com/vova1001/Website-Ylia-fitness/internal/model"
 )
 
@@ -15,6 +16,13 @@ func PostNewUser(ctx *gin.Context) {
 	err := ctx.ShouldBindJSON(&NewUser)
 	if err != nil {
 		ctx.JSON(400, gin.H{"err": "err json"})
+		return
 	}
+	err = h.RegisterNewUser(NewUser)
+	if err != nil {
+		ctx.JSON(401, gin.H{"err": "err handler register"})
+		return
+	}
+	ctx.JSON(200, gin.H{"Sucseseful": "User add table"})
 
 }
