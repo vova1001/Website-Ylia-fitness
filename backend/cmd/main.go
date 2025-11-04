fb44ab7ffa182f306caf006e29ebd5b4cec53d5c
vova <vovaoleshko05@mail.ru>
2025-11-04 23:09:24 +0300

    add metrics

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	d "github.com/vova1001/Website-Ylia-fitness/internal/database"
+	metrics "github.com/vova1001/Website-Ylia-fitness/internal/metrics"
 	rout "github.com/vova1001/Website-Ylia-fitness/internal/routes"
 )
 
 func main() {
-
+	metrics.MetricsInit()
 	d.DB_Conect()
 	r := gin.Default()
 	rout.RegisterRoutes(r)
--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -2,10 +2,14 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	h "github.com/vova1001/Website-Ylia-fitness/internal/handlerJSON"
+	metrics "github.com/vova1001/Website-Ylia-fitness/internal/metrics"
 )
 
 func RegisterRoutes(r *gin.Engine) {
+	r.Use(metrics.PrometheusMiddleware())
 	auth := r.Group("/auth")
 	auth.Use(h.JWT_Middleware())
 	{
@@ -19,4 +23,5 @@ func RegisterRoutes(r *gin.Engine) {
 	r.POST("/resetPassword", h.ResetPasswordJSON)
 	r.GET("/", h.GetHethJSON)
 	r.POST("/webhook", h.WebhookJSON)
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 }
