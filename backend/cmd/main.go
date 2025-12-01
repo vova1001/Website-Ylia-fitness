22b033b58d4ff3dc5f83ea071bb473e04026caa8
vova <vovaoleshko05@mail.ru>
2025-12-01 22:51:33 +0300

    Update main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,9 +5,23 @@ import (
 	d "github.com/vova1001/Website-Ylia-fitness/internal/database"
 	metrics "github.com/vova1001/Website-Ylia-fitness/internal/metrics"
 	rout "github.com/vova1001/Website-Ylia-fitness/internal/routes"
+
+	"log"
+	"net"
+	"time"
 )
 
 func main() {
+	ip := "109.235.165.99:443"
+	log.Println("Testing TCP connection to", ip)
+	conn, err := net.DialTimeout("tcp", ip, 10*time.Second)
+	if err != nil {
+		log.Println("TCP connection failed:", err)
+	} else {
+		log.Println("TCP connection succeeded")
+		conn.Close()
+	}
+
 	metrics.MetricsInit()
 	d.DB_Conect()
 	r := gin.Default()
